da542658b6e9e21e0d70f958dfe3476f0a50b2ce
Daniel Peralta <dannyland11@gmail.com>
2025-08-24 20:02:15 -0700

    hero section test

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,25 @@ function App() {
 
   return (
     <>
-      <h1>Daniel Peralta</h1>
+    <div className='font-lexend'>
+      <div className="text-center">
+        <h1 className='text-9xl font-bold'>DANIEL PERALTA</h1>
+      </div>
+      <div className='flex justify-center text-2xl font-light'>
+        <div className='flex flex-row divide-x-1'>
+          <a className='px-3'>PROJECTS</a>
+          <a className='px-3'>RESUME</a>
+          <a className='px-3'>CONTACT</a>
+        </div>
+      </div>
+      <div className='bg-[#eeeeee]'>
+        <div className="w-1/2 m-auto">
+          <div>
+            <h2 className='text-4xl'>ABOUT ME</h2>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
